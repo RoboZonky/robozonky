ba2cccd0a9f46260b00ba3b9b567df44d2430147
Lukáš Petrovický <lukas@petrovicky.net>
2016-05-21 16:16:23 +0200

    Invest only in the allowed increments of 200 (fixes #2) (#3)
    * Fix serialization problems when reading SIGNED investments from Zonky
    
    * Make sure we invest in increments of 200
    
    * Merge investment classes into one
    
    * Make sure Travis ignores the GPG plugin

--- robozonky-core/src/main/java/com/github/triceo/robozonky/remote/Investment.java
+++ robozonky-core/src/main/java/com/github/triceo/robozonky/remote/Investment.java
@@ -16,27 +16,43 @@
  */
 package com.github.triceo.robozonky.remote;
 
+import java.math.BigDecimal;
+import java.time.Instant;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlTransient;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 public class Investment {
 
-    private Loan loan;
-    private int loanId, amount;
+    private int id, loanId, amount, dpd, loanTermInMonth, currentTerm;
+    private String loanName, nickname, firstName, surname, paymentStatus;
+    private Instant investmentDate, nextPaymentDate;
+    private BigDecimal interestRate, paid, toPay, amountDue, paidInterest, dueInterest, paidPrincipal, duePrincipal, expectedInterest;
+    private Rating rating;
+
+    Investment() {
+        // for JAXB
+    }
 
     public Investment(final Loan loan, final int amount) {
-        this.loan = loan;
         this.loanId = loan.getId();
+        this.loanName = loan.getName();
+        this.nickname = loan.getNickName();
+        this.rating = loan.getRating();
+        this.loanTermInMonth = loan.getTermInMonths();
+        this.interestRate = loan.getInterestRate();
         this.amount = amount;
+        this.currentTerm = this.loanTermInMonth;
+        this.paymentStatus = "OK";
+        this.investmentDate = Instant.now();
+        this.paid = BigDecimal.ZERO;
+        this.paidPrincipal = BigDecimal.ZERO;
+        this.duePrincipal = BigDecimal.valueOf(amount);
     }
 
-    @XmlTransient
-    public Loan getLoan() {
-        return loan;
-    }
-
-    public void setLoan() {
-        throw new UnsupportedOperationException("Cannot set loan other than from the constructor.");
+    @XmlElement
+    public Rating getRating() {
+        return rating;
     }
 
     @XmlElement
@@ -49,11 +65,122 @@ public class Investment {
         return amount;
     }
 
+    @XmlElement
+    public int getId() {
+        return id;
+    }
+
+    @XmlElement
+    public int getDpd() {
+        return dpd;
+    }
+
+    @XmlElement
+    public int getLoanTermInMonth() {
+        return loanTermInMonth;
+    }
+
+    @XmlElement
+    public int getCurrentTerm() {
+        return currentTerm;
+    }
+
+    @XmlElement
+    public String getLoanName() {
+        return loanName;
+    }
+
+    @XmlElement
+    public String getNickname() {
+        return nickname;
+    }
+
+    @XmlElement
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @XmlElement
+    public String getSurname() {
+        return surname;
+    }
+
+    @XmlElement
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    @XmlElement
+    @JsonDeserialize(using = InstantDeserializer.class)
+    public Instant getInvestmentDate() {
+        return investmentDate;
+    }
+
+    @XmlElement
+    @JsonDeserialize(using = InstantDeserializer.class)
+    public Instant getNextPaymentDate() {
+        return nextPaymentDate;
+    }
+
+    @XmlElement
+    public BigDecimal getInterestRate() {
+        return interestRate;
+    }
+
+    @XmlElement
+    public BigDecimal getPaid() {
+        return paid;
+    }
+
+    @XmlElement
+    public BigDecimal getToPay() {
+        return toPay;
+    }
+
+    @XmlElement
+    public BigDecimal getAmountDue() {
+        return amountDue;
+    }
+
+    @XmlElement
+    public BigDecimal getPaidInterest() {
+        return paidInterest;
+    }
+
+    @XmlElement
+    public BigDecimal getDueInterest() {
+        return dueInterest;
+    }
+
+    @XmlElement
+    public BigDecimal getPaidPrincipal() {
+        return paidPrincipal;
+    }
+
+    @XmlElement
+    public BigDecimal getDuePrincipal() {
+        return duePrincipal;
+    }
+
+    @XmlElement
+    public BigDecimal getExpectedInterest() {
+        return expectedInterest;
+    }
+
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("Investment{");
-        sb.append("loanId=").append(loanId);
-        sb.append(", amount=").append(amount);
+        final StringBuilder sb = new StringBuilder("FullInvestment{");
+        sb.append("id=").append(id);
+        sb.append(", loanId=").append(this.getLoanId());
+        sb.append(", loanName='").append(loanName).append('\'');
+        sb.append(", amount=").append(this.getAmount());
+        sb.append(", rating=").append(this.getRating());
+        sb.append(", interestRate=").append(interestRate);
+        sb.append(", loanTermInMonth=").append(loanTermInMonth);
+        sb.append(", currentTerm=").append(currentTerm);
+        sb.append(", dpd=").append(dpd);
+        sb.append(", paymentStatus='").append(paymentStatus).append('\'');
+        sb.append(", amountDue=").append(amountDue);
         sb.append('}');
         return sb.toString();
     }
