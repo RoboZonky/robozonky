e21ffce4d39d0b67df07db7f22df1d78d84e87e9
Lukáš Petrovický <lpetrovi@redhat.com>
2016-06-18 11:07:30 +0200

    CommandLineInterface testing

--- robozonky-app/src/test/java/com/github/triceo/robozonky/app/CommandLineInterfaceTest.java
+++ robozonky-app/src/test/java/com/github/triceo/robozonky/app/CommandLineInterfaceTest.java
@@ -16,5 +16,87 @@
 
 package com.github.triceo.robozonky.app;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 public class CommandLineInterfaceTest extends AbstractNonExitingTest {
+
+    @Test(expected = RoboZonkyTestingExitException.class)
+    public void noArguments() {
+        CommandLineInterface.parse();
+    }
+
+    @Test
+    public void minimalStrategyDrivenCliWithKeyStoreAndToken() {
+        final CommandLineInterface cli = CommandLineInterface.parse("-s", "path", "-g", "key", "-p", "pwd", "-r", "5");
+        Assertions.assertThat(cli.getCliOperatingMode()).isEqualTo(Optional.of(OperatingMode.STRATEGY_DRIVEN));
+        Assertions.assertThat(cli.getStrategyConfigurationFilePath()).isPresent();
+        Assertions.assertThat(cli.getKeyStoreLocation()).isPresent();
+        Assertions.assertThat(cli.isTokenEnabled()).isTrue();
+        Assertions.assertThat(cli.getTokenRefreshBeforeExpirationInSeconds()).isPresent();
+        Assertions.assertThat(cli.getPassword()).isNotNull();
+        Assertions.assertThat(cli.getUsername()).isEmpty();
+    }
+
+    @Test
+    public void minimalStrategyDrivenCliWithKeyStoreAndTokenNoRefresh() {
+        final CommandLineInterface cli = CommandLineInterface.parse("-s", "path", "-g", "key", "-p", "pwd", "-r");
+        Assertions.assertThat(cli.getCliOperatingMode()).isEqualTo(Optional.of(OperatingMode.STRATEGY_DRIVEN));
+        Assertions.assertThat(cli.getStrategyConfigurationFilePath()).isPresent();
+        Assertions.assertThat(cli.getKeyStoreLocation()).isPresent();
+        Assertions.assertThat(cli.isTokenEnabled()).isTrue();
+        Assertions.assertThat(cli.getTokenRefreshBeforeExpirationInSeconds()).isEmpty();
+        Assertions.assertThat(cli.getPassword()).isNotNull();
+        Assertions.assertThat(cli.getUsername()).isEmpty();
+    }
+
+    @Test
+    public void minimalStrategyDrivenCli() {
+        final CommandLineInterface cli = CommandLineInterface.parse("-s", "somePath", "-u", "user", "-p", "password");
+        Assertions.assertThat(cli.getCliOperatingMode()).isEqualTo(Optional.of(OperatingMode.STRATEGY_DRIVEN));
+        Assertions.assertThat(cli.getStrategyConfigurationFilePath()).isPresent();
+        Assertions.assertThat(cli.getUsername()).isPresent();
+        Assertions.assertThat(cli.getPassword()).isNotNull();
+        Assertions.assertThat(cli.getLoanId()).isEmpty();
+        Assertions.assertThat(cli.getLoanAmount()).isEmpty();
+        Assertions.assertThat(cli.isDryRun()).isFalse();
+        Assertions.assertThat(cli.getDryRunBalance()).isEmpty();
+        Assertions.assertThat(cli.getKeyStoreLocation()).isEmpty();
+        Assertions.assertThat(cli.getTokenRefreshBeforeExpirationInSeconds()).isEmpty();
+    }
+
+    @Test
+    public void minimalUserDrivenCli() {
+        final CommandLineInterface cli = CommandLineInterface.parse("-l", "1", "-a", "1000", "-u", "user", "-p", "pwd");
+        Assertions.assertThat(cli.getCliOperatingMode()).isEqualTo(Optional.of(OperatingMode.USER_DRIVEN));
+        Assertions.assertThat(cli.getLoanId()).isPresent();
+        Assertions.assertThat(cli.getLoanAmount()).isPresent();
+        Assertions.assertThat(cli.getUsername()).isPresent();
+        Assertions.assertThat(cli.getPassword()).isNotNull();
+        Assertions.assertThat(cli.getStrategyConfigurationFilePath()).isEmpty();
+        Assertions.assertThat(cli.isDryRun()).isFalse();
+        Assertions.assertThat(cli.getDryRunBalance()).isEmpty();
+        Assertions.assertThat(cli.getKeyStoreLocation()).isEmpty();
+        Assertions.assertThat(cli.getTokenRefreshBeforeExpirationInSeconds()).isEmpty();
+        Assertions.assertThat(cli.isTokenEnabled()).isFalse();
+    }
+
+    @Test
+    public void userDrivenCliWrongAmount() {
+        final CommandLineInterface cli = CommandLineInterface.parse("-l", "1", "-a", "10a0", "-u", "user", "-p", "pwd");
+        Assertions.assertThat(cli.getCliOperatingMode()).isEqualTo(Optional.of(OperatingMode.USER_DRIVEN));
+        Assertions.assertThat(cli.getLoanId()).isPresent();
+        Assertions.assertThat(cli.getUsername()).isPresent();
+        Assertions.assertThat(cli.getLoanAmount()).isEmpty();
+        Assertions.assertThat(cli.getPassword()).isNotNull();
+        Assertions.assertThat(cli.getStrategyConfigurationFilePath()).isEmpty();
+        Assertions.assertThat(cli.isDryRun()).isFalse();
+        Assertions.assertThat(cli.getDryRunBalance()).isEmpty();
+        Assertions.assertThat(cli.getKeyStoreLocation()).isEmpty();
+        Assertions.assertThat(cli.getTokenRefreshBeforeExpirationInSeconds()).isEmpty();
+        Assertions.assertThat(cli.isTokenEnabled()).isFalse();
+    }
+
 }
