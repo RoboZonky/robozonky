8e8d93a7a6a1141f7b0bebc66ea36baf514a0a56
Lukáš Petrovický <lukas@petrovicky.net>
2016-07-19 19:43:30 +0200

    Logging needs to be configurable from the outside
    Fixes #29

--- robozonky-app/src/main/assembly/filtered-resources/robozonky.sh
+++ robozonky-app/src/main/assembly/filtered-resources/robozonky.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
-$JAVA_HOME/bin/java -Dfile.encoding="UTF-8" -Djava.net.preferIPv4Stack=true -Xmx64m -jar $BASEDIR/robozonky-app-${project.version}.jar "$@"
+$JAVA_HOME/bin/java -Dlogback.configurationFile=logback.xml -Dfile.encoding="UTF-8" -Djava.net.preferIPv4Stack=true -Xmx64m -jar $BASEDIR/robozonky-app-${project.version}.jar "$@"
