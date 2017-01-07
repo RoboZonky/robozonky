138e66267963417d4967438c97456fc1c9237917
Lukáš Petrovický <lukas@petrovicky.net>
2017-01-07 20:41:53 +0100

    Properly set env vars on Windows (fixes #81)

--- robozonky-app/src/main/assembly/filtered-resources/robozonky.bat
+++ robozonky-app/src/main/assembly/filtered-resources/robozonky.bat
@@ -1,2 +1,2 @@
-SET JAVA_OPTS = "%JAVA_OPTS% -Dlogback.configurationFile=logback.xml -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
+set "JAVA_OPTS=%JAVA_OPTS% -Dlogback.configurationFile=logback.xml -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
 java %JAVA_OPTS% -jar robozonky-app-${project.version}.jar %*
