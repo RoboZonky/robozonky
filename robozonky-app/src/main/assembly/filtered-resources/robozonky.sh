08c2c5b8affd7acac0819bb5d07df628f7f21629
Lukáš Petrovický <lukas@petrovicky.net>
2017-02-08 19:29:40 +0100

    Make remote JMX connections actually work

--- robozonky-app/src/main/assembly/filtered-resources/robozonky.sh
+++ robozonky-app/src/main/assembly/filtered-resources/robozonky.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false"
-JAVA_OPTS="$JAVA_OPTS -Dlogback.configurationFile=logback.xml -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
-$JAVA_HOME/bin/java $JAVA_OPTS -jar bin/robozonky-app-${project.version}.jar @robozonky.cli
+ROBOZONKY_PORT=${ROBOZONKY_PORT:-7091}
+ROBOZONKY_IP=${ROBOZONKY_IP:-localhost}
+JMX_OPTS="-Djava.rmi.server.hostname=$ROBOZONKY_IP -Dcom.sun.management.jmxremote.port=$ROBOZONKY_PORT -Dcom.sun.management.jmxremote.rmi.port=$ROBOZONKY_PORT -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+ROBOZONKY_OPTS="$JMX_OPTS $JAVA_OPTS -Dlogback.configurationFile=logback.xml -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
+$JAVA_HOME/bin/java $ROBOZONKY_OPTS -jar bin/robozonky-app-${project.version}.jar @robozonky.cli
