ebec7094252a13a69eb7ae4b1229622d64dc2ed7
Lukáš Petrovický <lukas@petrovicky.net>
2017-02-23 21:37:43 +0100

    Scripts need to substitute whatever command line arguments

--- robozonky-app/src/main/assembly/filtered-resources/robozonky.bat
+++ robozonky-app/src/main/assembly/filtered-resources/robozonky.bat
@@ -2,4 +2,4 @@ if not defined ROBOZONKY_PORT set ROBOZONKY_PORT=7091
 if not defined ROBOZONKY_IP set ROBOZONKY_IP=localhost
 set "JMX_OPTS=-Djava.rmi.server.hostname=%ROBOZONKY_IP% -Dcom.sun.management.jmxremote.port=%ROBOZONKY_PORT% -Dcom.sun.management.jmxremote.rmi.port=%ROBOZONKY_PORT% -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 set "ROBOZONKY_OPTS=%JMX_OPTS% %JAVA_OPTS% -Dlogback.configurationFile=logback.xml -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
-java %ROBOZONKY_OPTS% -jar bin/robozonky-app-${project.version}.jar @robozonky.cli
+java %ROBOZONKY_OPTS% -jar bin/robozonky-app-${project.version}.jar %*
--- robozonky-app/src/main/assembly/filtered-resources/robozonky.sh
+++ robozonky-app/src/main/assembly/filtered-resources/robozonky.sh
@@ -3,4 +3,4 @@ ROBOZONKY_PORT=${ROBOZONKY_PORT:-7091}
 ROBOZONKY_IP=${ROBOZONKY_IP:-localhost}
 JMX_OPTS="-Djava.rmi.server.hostname=$ROBOZONKY_IP -Dcom.sun.management.jmxremote.port=$ROBOZONKY_PORT -Dcom.sun.management.jmxremote.rmi.port=$ROBOZONKY_PORT -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 ROBOZONKY_OPTS="$JMX_OPTS $JAVA_OPTS -Dlogback.configurationFile=logback.xml -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
-$JAVA_HOME/bin/java $ROBOZONKY_OPTS -jar bin/robozonky-app-${project.version}.jar @robozonky.cli
+$JAVA_HOME/bin/java $ROBOZONKY_OPTS -jar bin/robozonky-app-${project.version}.jar "$@"
