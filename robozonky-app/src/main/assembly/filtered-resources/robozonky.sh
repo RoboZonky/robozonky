aaea5442a63a7e70b87f63b183ff7d85007a41f7
Ondřej Nový <novy@ondrej.org>
2017-02-24 15:02:19 +0100

    Search for jar file inside BASEDIR (#98)
    This used to work on 2.0 and then, for some reason, it was removed.

--- robozonky-app/src/main/assembly/filtered-resources/robozonky.sh
+++ robozonky-app/src/main/assembly/filtered-resources/robozonky.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
 ROBOZONKY_PORT=${ROBOZONKY_PORT:-7091}
 ROBOZONKY_IP=${ROBOZONKY_IP:-localhost}
 JMX_OPTS="-Djava.rmi.server.hostname=$ROBOZONKY_IP -Dcom.sun.management.jmxremote.port=$ROBOZONKY_PORT -Dcom.sun.management.jmxremote.rmi.port=$ROBOZONKY_PORT -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 ROBOZONKY_OPTS="$JMX_OPTS $JAVA_OPTS -Dlogback.configurationFile=logback.xml -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
-$JAVA_HOME/bin/java $ROBOZONKY_OPTS -jar bin/robozonky-app-${project.version}.jar "$@"
+$JAVA_HOME/bin/java $ROBOZONKY_OPTS -jar ${BASEDIR}/bin/robozonky-app-${project.version}.jar "$@"
