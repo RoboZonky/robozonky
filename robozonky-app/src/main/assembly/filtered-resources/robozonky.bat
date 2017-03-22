ca12b8bcf0c8b63cde4e15e2914959155fde4277
Lukáš Petrovický <lpetrovi@redhat.com>
2017-03-22 19:02:00 +0100

    Move to G1 garbage collector

--- robozonky-app/src/main/assembly/filtered-resources/robozonky.bat
+++ robozonky-app/src/main/assembly/filtered-resources/robozonky.bat
@@ -1,2 +1,2 @@
-set "ROBOZONKY_OPTS=%JAVA_OPTS% -Dlogback.configurationFile=logback.xml -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
+set "ROBOZONKY_OPTS=-XX:+UseG1GC %JAVA_OPTS% -Dlogback.configurationFile=logback.xml -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
 java %ROBOZONKY_OPTS% -jar bin/robozonky-app-${project.version}.jar %*
