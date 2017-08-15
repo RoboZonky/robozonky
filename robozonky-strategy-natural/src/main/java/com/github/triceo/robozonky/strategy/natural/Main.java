742cac81b643cc66b93e5d64c6d154d93bc36150
Lukáš Petrovický <lukas@petrovicky.net>
2017-08-15 08:01:12 +0200

    Make natural strategy JAR executable (#142)
    This will help with strategy debugging.

---
+++ robozonky-strategy-natural/src/main/java/com/github/triceo/robozonky/strategy/natural/Main.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 Lukáš Petrovický
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.triceo.robozonky.strategy.natural;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * Simplifies testing of strategies by enabling parser to be launched directly from the command line.
+ */
+public class Main {
+
+    public static void main(final String... args) {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("The application expects exactly one argument: the strategy file.");
+        }
+        final File f = new File(args[0]);
+        if (!f.exists() || !f.canRead()) {
+            throw new IllegalArgumentException("Can not read strategy file: " + f.getAbsolutePath());
+        }
+        try (final InputStream s = new BufferedInputStream(new FileInputStream(f))) {
+            System.out.println("Will read: " + f.getAbsolutePath());
+            NaturalLanguageStrategyService.parseWithAntlr(s);
+        } catch (final Exception ex) {
+            throw new IllegalStateException("Failed parsing strategy.", ex);
+        }
+    }
+}
