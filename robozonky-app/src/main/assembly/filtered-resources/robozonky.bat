f0fa821b7e86d236a3e2a643ea8b6e2a5ec0f7e9
Lukáš Petrovický <lpetrovi@redhat.com>
2017-11-12 17:26:11 +0100

    Remove JAVA_HOME detection (fixes #170)
    It was only causing trouble. Sometimes it went undetected, breaking the
    run script. Other times, it would break the scripts when the JRE was
    updated to a new version and the old JAVA_HOME was no longer valid.
    
    (cherry picked from commit 5d9f637)

--- robozonky-app/src/main/assembly/filtered-resources/robozonky.bat
+++ robozonky-app/src/main/assembly/filtered-resources/robozonky.bat
@@ -1,3 +1,3 @@
 set "ROBOZONKY_OPTS=%JAVA_OPTS% -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
 set "HERE=%~dp0%"
-"%JAVA_HOME%\bin\java" %ROBOZONKY_OPTS% -jar %HERE%bin\robozonky-app-${project.version}.jar %*
+java %ROBOZONKY_OPTS% -jar %HERE%bin\robozonky-app-${project.version}.jar %*
--- robozonky-app/src/main/assembly/filtered-resources/robozonky.sh
+++ robozonky-app/src/main/assembly/filtered-resources/robozonky.sh
@@ -4,4 +4,4 @@ if [ -f ${HERE}/Dist/bin/robozonky-app-${project.version}.jar ]; then
     HERE=$HERE/Dist
 fi
 ROBOZONKY_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
-$JAVA_HOME/bin/java $ROBOZONKY_OPTS -jar ${HERE}/bin/robozonky-app-${project.version}.jar "$@"
+java $ROBOZONKY_OPTS -jar ${HERE}/bin/robozonky-app-${project.version}.jar "$@"
