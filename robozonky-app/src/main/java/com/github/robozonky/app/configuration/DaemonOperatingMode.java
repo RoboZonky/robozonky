24af34257a4f0065e5f164bc67bc3117c09d12cb
Lukáš Petrovický <lpetrovi@redhat.com>
2017-12-01 19:35:52 +0100

    Add one FIXME

--- robozonky-app/src/main/java/com/github/robozonky/app/configuration/DaemonOperatingMode.java
+++ robozonky-app/src/main/java/com/github/robozonky/app/configuration/DaemonOperatingMode.java
@@ -81,6 +81,7 @@ class DaemonOperatingMode extends OperatingMode {
                                                                       marketplace.getPrimaryMarketplaceCheckDelay(),
                                                                       marketplace.getSecondaryMarketplaceCheckDelay());
                     // initialize SessionInfo before the robot potentially sends the first notification
+                    // FIXME this sends an e-mail about robot being ready, which is not yet the case here
                     Events.fire(new RoboZonkyInitializedEvent(), new SessionInfo(secretProvider.getUsername()));
                     // only schedule internal data updates after daemon had a chance to initialize...
                     final Scheduler scheduler = Scheduler.inBackground();
