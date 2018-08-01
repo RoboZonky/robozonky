a3a7759d0e7fd2b4d64ea63b373f9a93b12b391e
Lukáš Petrovický <lukas@petrovicky.net>
2018-08-01 19:07:01 +0200

    Implement HTTP proxy settings (fixes #286)

--- robozonky-common/src/test/java/com/github/robozonky/common/remote/ApiProviderTest.java
+++ robozonky-common/src/test/java/com/github/robozonky/common/remote/ApiProviderTest.java
@@ -22,8 +22,11 @@ import java.util.function.Function;
 import com.github.robozonky.api.remote.entities.ZonkyApiToken;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class ApiProviderTest {
 
