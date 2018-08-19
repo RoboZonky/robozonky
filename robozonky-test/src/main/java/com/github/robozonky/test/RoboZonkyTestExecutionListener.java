7ef81d18955dbf8e8e27ddaa9ae9192a22e7e60b
Lukáš Petrovický <lukas@petrovicky.net>
2018-08-19 16:05:55 +0200

    Send all logging to sysout to ease debugging in CI

---
+++ robozonky-test/src/main/java/com/github/robozonky/test/RoboZonkyTestExecutionListener.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 The RoboZonky Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.robozonky.test;
+
+import org.junit.platform.engine.TestExecutionResult;
+import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.TestIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class RoboZonkyTestExecutionListener implements TestExecutionListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RoboZonkyTestExecutionListener.class);
+
+    private static String identifyTest(final TestIdentifier testIdentifier) {
+        return testIdentifier.getUniqueId();
+    }
+
+    @Override
+    public void executionSkipped(final TestIdentifier testIdentifier, final String reason) {
+        LOGGER.info("Skipped {}. Cause: {}.", identifyTest(testIdentifier), reason);
+    }
+
+    @Override
+    public void executionStarted(final TestIdentifier testIdentifier) {
+        LOGGER.info("Started {}.", identifyTest(testIdentifier));
+    }
+
+    @Override
+    public void executionFinished(final TestIdentifier testIdentifier, final TestExecutionResult testExecutionResult) {
+        LOGGER.info("Finished {}.", identifyTest(testIdentifier));
+    }
+}
