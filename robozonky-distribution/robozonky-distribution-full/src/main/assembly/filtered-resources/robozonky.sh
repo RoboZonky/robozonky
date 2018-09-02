87263a49bbd425ebad0f48be0f545568bc94b3f3
vvvlc <vitezslav+github@vvvlcek.info>
2018-09-02 20:21:04 +0200

    Update robozonky.sh
    added comment with link with details

--- robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.sh
+++ robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.sh
@@ -4,4 +4,5 @@ if [ -f ${HERE}/Dist/bin/robozonky-app-${project.version}.jar ]; then
     HERE=$HERE/Dist
 fi
 ROBOZONKY_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
+#added exec to propagate signals when docker stop is issued, more details https://github.com/RoboZonky/robozonky/pull/305
 exec java $ROBOZONKY_OPTS -jar ${HERE}/bin/robozonky-app-${project.version}.jar "$@"
