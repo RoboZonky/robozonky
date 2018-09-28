febb91088f570675f83bcbe6de5a32577c228df3
Lukáš Petrovický <lukas@petrovicky.net>
2018-09-28 22:38:42 +0200

    Increase mutation coverage in robozonky-common

--- robozonky-common/src/test/java/com/github/robozonky/util/SchedulerTest.java
+++ robozonky-common/src/test/java/com/github/robozonky/util/SchedulerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 The RoboZonky Project
+ * Copyright 2018 The RoboZonky Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package com.github.robozonky.util;
 
+import java.time.Duration;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledFuture;
@@ -48,6 +49,18 @@ class SchedulerTest {
     void run() throws InterruptedException, ExecutionException, TimeoutException {
         try (final Scheduler s = Schedulers.INSTANCE.create()) {
             final Future<?> f = s.run(REFRESHABLE);
+            SoftAssertions.assertSoftly(softly -> {
+                softly.assertThat((Future<?>) f).isNotNull();
+                softly.assertThat(s.isSubmitted(REFRESHABLE)).isFalse();
+            });
+            f.get(1, TimeUnit.MINUTES); // make sure it was executed
+        }
+    }
+
+    @Test
+    void runWithDelay() throws InterruptedException, ExecutionException, TimeoutException {
+        try (final Scheduler s = Schedulers.INSTANCE.create()) {
+            final Future<?> f = s.run(REFRESHABLE, Duration.ofSeconds(1));
             SoftAssertions.assertSoftly(softly -> {
                 softly.assertThat((Future<?>) f).isNotNull();
                 softly.assertThat(s.isSubmitted(REFRESHABLE)).isFalse();
