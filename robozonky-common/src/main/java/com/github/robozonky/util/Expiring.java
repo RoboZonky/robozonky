a202580139c3bfbaef9fbbe5a584fdab4360fc34
Lukáš Petrovický <lukas@petrovicky.net>
2018-11-25 15:30:37 +0100

    Use Vavr's Tuple

--- robozonky-common/src/main/java/com/github/robozonky/util/Expiring.java
+++ robozonky-common/src/main/java/com/github/robozonky/util/Expiring.java
@@ -22,13 +22,15 @@ import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class Expiring<T> implements Supplier<Optional<T>> {
 
     protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
-    private final AtomicReference<Tuple<T>> value = new AtomicReference<>();
+    private final AtomicReference<Tuple2<T, Instant>> value = new AtomicReference<>();
     private final Duration expireAfter;
     private final Runnable runWhenUpdated;
 
@@ -47,43 +49,20 @@ public abstract class Expiring<T> implements Supplier<Optional<T>> {
 
     @Override
     public Optional<T> get() {
-        final Tuple<T> result = value.updateAndGet(old -> {
-            if (old == null || !old.getRetrievedOn().plus(expireAfter).isAfter(Instant.now())) {
+        final Tuple2<T, Instant> result = value.updateAndGet(old -> {
+            if (old == null || !old._2().plus(expireAfter).isAfter(Instant.now())) {
                 LOGGER.trace("Retrieving new value.");
                 return retrieve().map(v -> {
                     LOGGER.debug("Retrieved new value: {}.", v);
                     runWhenUpdated.run();
-                    return new Tuple<>(v);
+                    return Tuple.of(v, Instant.now());
                 }).orElse(old);
             } else {
                 LOGGER.trace("Keeping existing value.");
                 return old;
             }
         });
-        return result == null ? Optional.empty() : Optional.ofNullable(result.getValue());
-    }
-
-    private static final class Tuple<X> {
-
-        private final X value;
-        private final Instant retrievedOn;
-
-        public Tuple(final X value, final Instant retreivedOn) {
-            this.value = value;
-            this.retrievedOn = retreivedOn;
-        }
-
-        public Tuple(final X value) {
-            this(value, Instant.now());
-        }
-
-        public X getValue() {
-            return value;
-        }
-
-        public Instant getRetrievedOn() {
-            return retrievedOn;
-        }
+        return result == null ? Optional.empty() : Optional.ofNullable(result._1());
     }
 
 }
