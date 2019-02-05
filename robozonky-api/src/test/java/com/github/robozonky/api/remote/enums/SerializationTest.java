42b116a97a387809cb23470f032df6fb5ff133a0
Lukáš Petrovický <lukas@petrovicky.net>
2019-02-05 21:12:01 +0100

    Make regions fault-tolerant (fixes #385)

--- robozonky-api/src/test/java/com/github/robozonky/api/remote/enums/SerializationTest.java
+++ robozonky-api/src/test/java/com/github/robozonky/api/remote/enums/SerializationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 The RoboZonky Project
+ * Copyright 2019 The RoboZonky Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -78,7 +78,7 @@ class SerializationTest {
             tests.add(dynamicTest(deserializeTestName(toSerialize), () -> deserialize(serialized, toSerialize)));
         }
         // test that deserialization of invalid value properly fails
-        Stream.of(Region.class, Purpose.class, MainIncomeType.class)
+        Stream.of(Purpose.class, MainIncomeType.class)
                 .forEach(clz -> tests.add(dynamicTest("invalid " + clz.getSimpleName(),
                                                       () -> deserialize(clz))));
         return tests;
