9a32775de27970c75bc0065061edb37532c6f0f1
Lukáš Petrovický <lukas@petrovicky.net>
2019-02-24 16:43:06 +0100

    Reimplement ThreadPoolExecutorBasedScheduler to support proper delays

--- robozonky-common/src/main/java/com/github/robozonky/common/async/ThreadPoolExecutorBasedScheduler.java
+++ robozonky-common/src/main/java/com/github/robozonky/common/async/ThreadPoolExecutorBasedScheduler.java
@@ -18,15 +18,39 @@ package com.github.robozonky.common.async;
 
 import java.time.Duration;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import com.github.robozonky.internal.api.Settings;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * This attempts to provide functionality similar to {@link ScheduledExecutorService}, but with a fully configurable
+ * underlying {@link ThreadPoolExecutor}. ({@link ScheduledThreadPoolExecutor} does not really allow for scaling the
+ * pool size as cached thread pool does.
+ * <p>
+ * This is how it works:
+ * <ul>
+ * <li>Task scheduling is requested via {@link #submit(Runnable, Duration, Duration)}. The task has a delay in which it
+ * should be started, as well as a delay between the finish of one tasks and the start of the next consecutive
+ * task.</li>
+ * <li>The task itself ("original task") will be executed on a given {@link ExecutorService}, but in order to implement
+ * the other properties, we need to implement a level of indirection via {@link ScheduledExecutorService}.</li>
+ * <li>First, a task is scheduled in the {@link ScheduledExecutorService}, which only tells the {@link ExecutorService}
+ * to schedule the original task after the delay has passed.</li>
+ * <li>At the end of original task's execution, another request is sent to the {@link ScheduledExecutorService} to again
+ * schedule the original task with the {@link ExecutorService}, this time with the delay equal to the delay between the
+ * tasks, as initially specified.</li>
+ * <li>The {@link ScheduledFuture} returned by {@link #submit(Runnable, Duration)} will always refer to the active
+ * request on the {@link ScheduledExecutorService} - that is, if a new request has already been sent, the
+ * {@link ScheduledFuture} will be referring to that new request. This is only logical, since at that point, the old
+ * request is already finished.</li>
+ * </ul>
+ */
 final class ThreadPoolExecutorBasedScheduler implements Scheduler {
 
     private static final Logger LOGGER = LogManager.getLogger(SchedulerImpl.class);
@@ -42,9 +66,37 @@ final class ThreadPoolExecutorBasedScheduler implements Scheduler {
         this.onClose = onClose;
     }
 
-    private Future<?> submitToExecutor(final Runnable toSchedule) {
+    private ScheduledFuture<?> schedule(final Runnable toSchedule, final Duration delayInBetween) {
+        LOGGER.debug("Scheduling {} to happen after {} ms.", toSchedule, delayInBetween.toMillis());
+        return schedulingExecutor.schedule(toSchedule, delayInBetween.toNanos(), TimeUnit.NANOSECONDS);
+    }
+
+    private void schedule(final DelegatingScheduledFuture<?> delegating, final Runnable toSchedule,
+                          final Duration initialDelay, final Duration delayInBetween) {
+        final Runnable toSubmit = () -> submitToExecutor(delegating, toSchedule, delayInBetween);
+        final ScheduledFuture<?> f = schedule(toSubmit, initialDelay);
+        delegating.setCurrent(f);
+    }
+
+    private void schedule(final DelegatingScheduledFuture<?> delegating, final Runnable toSchedule,
+                          final Duration delayInBetween) {
+        schedule(delegating, toSchedule, delayInBetween, delayInBetween);
+    }
+
+    private void resubmittingTask(final DelegatingScheduledFuture<?> delegating, final Runnable toSchedule,
+                                  final Duration delayInBetween) {
+        LOGGER.trace("Running {}.", toSchedule);
+        toSchedule.run(); // run the task that was scheduled
+        // schedule its repeat with a given delay
+        schedule(delegating, toSchedule, delayInBetween);
+        LOGGER.trace("Rescheduling complete: {}.", toSchedule);
+    }
+
+    private void submitToExecutor(final DelegatingScheduledFuture<?> delegating, final Runnable toSchedule,
+                                  final Duration delayInBetween) {
+        final Runnable r = () -> resubmittingTask(delegating, toSchedule, delayInBetween);
         LOGGER.debug("Submitting {} for actual execution.", toSchedule);
-        return executor.submit(toSchedule);
+        executor.submit(r);
     }
 
     @Override
@@ -54,7 +106,7 @@ final class ThreadPoolExecutorBasedScheduler implements Scheduler {
 
     @Override
     public ScheduledFuture<?> submit(final Runnable toSchedule, final Duration delayInBetween) {
-        return submit(toSchedule, REFRESH, Duration.ZERO);
+        return submit(toSchedule, delayInBetween, Duration.ZERO);
     }
 
     @Override
@@ -62,9 +114,9 @@ final class ThreadPoolExecutorBasedScheduler implements Scheduler {
                                      final Duration firstDelay) {
         LOGGER.debug("Scheduling {} every {} ms, starting in {} ms.", toSchedule, delayInBetween.toMillis(),
                      firstDelay.toMillis());
-        final Runnable toSubmit = () -> submitToExecutor(toSchedule);
-        return schedulingExecutor.scheduleWithFixedDelay(toSubmit, firstDelay.toNanos(), delayInBetween.toNanos(),
-                                                         TimeUnit.NANOSECONDS);
+        final DelegatingScheduledFuture<?> delegating = new DelegatingScheduledFuture<>();
+        schedule(delegating, toSchedule, firstDelay, delayInBetween);
+        return delegating;
     }
 
     @Override
