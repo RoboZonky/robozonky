28dcbc4ad7ef07fa72ae5268efa501073b22bec3
Lukáš Petrovický <lukas@petrovicky.net>
2019-03-02 08:43:23 +0100

    Enable flight recording

--- robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.bat
+++ robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.bat
@@ -1,3 +1,7 @@
-set "ROBOZONKY_OPTS=%JAVA_OPTS% -XX:+ExitOnOutOfMemoryError -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
+set "ROBOZONKY_OPTS=%JAVA_OPTS%^
+    -XX:+ExitOnOutOfMemoryError^
+    -Dfile.encoding=UTF-8^
+    -Djava.net.preferIPv4Stack=true^
+    -XX:StartFlightRecording=disk=true,dumponexit=true,maxage=1d,path-to-gc-roots=true"
 set "HERE=%~dp0%"
 java %ROBOZONKY_OPTS% -jar %HERE%bin\robozonky-app-${project.version}.jar %*
