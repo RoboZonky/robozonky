16b26993fff0acf4e8515c870465fa6262b328e1
Lukáš Petrovický <lukas@petrovicky.net>
2019-05-06 14:45:48 +0200

    Make sure RoboZonky version is present (fixes #423)

--- robozonky-api/src/main/java/com/github/robozonky/internal/api/Defaults.java
+++ robozonky-api/src/main/java/com/github/robozonky/internal/api/Defaults.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 The RoboZonky Project
+ * Copyright 2019 The RoboZonky Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,30 +16,48 @@
 
 package com.github.robozonky.internal.api;
 
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.time.ZoneId;
 import java.util.Locale;
+import java.util.Properties;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Carries application constants (such as version) and desired environmental settings (such as charset or locale).
  */
 public final class Defaults {
 
+    private static final Logger LOGGER = LogManager.getLogger(Defaults.class);
+
     public static final String MEDIA_TYPE = MediaType.APPLICATION_JSON + "; charset=UTF-8";
     public static final Locale LOCALE = Locale.forLanguageTag("cs-CZ");
     public static final Charset CHARSET = StandardCharsets.UTF_8;
     public static final ZoneId ZONE_ID = ZoneId.of("Europe/Prague");
     /**
-     * Will be null when running from IDE, Maven Surefire etc.; no JAR information at the time.
+     * May be null when running from IDE, Maven Surefire etc.; Maven resource filtering may not be applied there.
      */
-    public static final String ROBOZONKY_VERSION = Defaults.class.getPackage().getImplementationVersion();
+    public static final String ROBOZONKY_VERSION = getVersion();
     public static final String ROBOZONKY_URL = "http://www.robozonky.cz";
     public static final String ROBOZONKY_USER_AGENT =
             "RoboZonky/" + Defaults.ROBOZONKY_VERSION + " (" + Defaults.ROBOZONKY_URL + ")";
 
     private Defaults() {
         // no instances
+    }
+
+    private static String getVersion() {
+        try (final InputStream inputStream = Defaults.class.getResourceAsStream("version.properties")) {
+            final Properties properties = new Properties();
+            properties.load(inputStream);
+            return properties.getProperty("version", "missing");
+        } catch (final Exception ex) {
+            LOGGER.debug("Failed reading RoboZonky version.", ex);
+            return "unknown";
+        }
     }
 }
