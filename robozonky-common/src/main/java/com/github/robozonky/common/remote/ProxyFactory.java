1b9852d8c2b555db29be7df134cc65d3371569df
Lukáš Petrovický <lukas@petrovicky.net>
2019-05-08 09:43:02 +0200

    Improve HTTP proxy setting mechanism

--- robozonky-common/src/main/java/com/github/robozonky/common/remote/ProxyFactory.java
+++ robozonky-common/src/main/java/com/github/robozonky/common/remote/ProxyFactory.java
@@ -23,7 +23,6 @@ import com.github.robozonky.internal.api.Settings;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 
 final class ProxyFactory {
 
@@ -40,16 +39,20 @@ final class ProxyFactory {
         LOGGER.debug("Set socket timeout to {} ms.", socketTimeout);
         final long connectionTimeout = settings.getConnectionTimeout().toMillis();
         LOGGER.debug("Set connection timeout to {} ms.", connectionTimeout);
-        final ResteasyClientBuilder builder = (ResteasyClientBuilder) ClientBuilder.newBuilder()
+        final ClientBuilder builder = ClientBuilder.newBuilder()
                 .readTimeout(socketTimeout, TimeUnit.MILLISECONDS)
                 .connectTimeout(connectionTimeout, TimeUnit.MILLISECONDS);
-        // setup HTTP proxy when required
+        /*
+         * setup HTTP proxy when required (see
+         * http://docs.jboss.org/resteasy/docs/4.0.0.Final/userguide/html/RESTEasy_Client_Framework.html#http_proxy)
+         */
         settings.getHttpsProxyHostname().ifPresent(host -> {
             final int port = settings.getHttpsProxyPort();
-            builder.defaultProxy(host, port);
+            builder.property("org.jboss.resteasy.jaxrs.client.proxy.host", host)
+                    .property("org.jboss.resteasy.jaxrs.client.proxy.port", port);
             LOGGER.debug("Set HTTP proxy to {}:{}.", host, port);
         });
-        return builder.build();
+        return (ResteasyClient)builder.build();
     }
 
     public static <T> T newProxy(final ResteasyClient client, final RoboZonkyFilter filter, final Class<T> api,
