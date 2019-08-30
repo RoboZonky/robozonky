8d68eb074f40940323476ecdb487c55668cacfc0
Lukáš Petrovický <lukas@petrovicky.net>
2019-08-30 17:58:41 +0200

    Upgrade Maven Jar Plugin to 3.1.2
    MJAR-238 is fixed, allowing us to not specify main class in run scripts.

--- robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.bat
+++ robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.bat
@@ -1,3 +1,3 @@
 set "ROBOZONKY_OPTS=%JAVA_OPTS% -XX:+ExitOnOutOfMemoryError -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
 set "HERE=%~dp0%"
-java %ROBOZONKY_OPTS% --module-path %HERE%\bin --module com.github.robozonky.app/com.github.robozonky.app.App %*
+java %ROBOZONKY_OPTS% --module-path %HERE%\bin --module com.github.robozonky.app %*
--- robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.sh
+++ robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.sh
@@ -5,4 +5,4 @@ if [ -f ${HERE}/Dist/bin/robozonky-app-${project.version}.jar ]; then
 fi
 ROBOZONKY_OPTS="$JAVA_OPTS -XX:+ExitOnOutOfMemoryError -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
 #added exec to propagate signals when docker stop is issued, more details https://github.com/RoboZonky/robozonky/pull/305
-exec java $ROBOZONKY_OPTS --module-path ${HERE}/bin --module com.github.robozonky.app/com.github.robozonky.app.App "$@"
+exec java $ROBOZONKY_OPTS --module-path ${HERE}/bin --module com.github.robozonky.app "$@"
