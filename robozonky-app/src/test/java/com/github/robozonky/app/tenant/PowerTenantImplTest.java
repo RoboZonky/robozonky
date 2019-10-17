4679db8554659c52c839f82a79deee8c04f3176f
Lukáš Petrovický <lukas@petrovicky.net>
2019-10-17 18:04:35 +0200

    Revert "Refresh token before robot shutdown"
    This reverts commit 4ad93e79

--- robozonky-app/src/test/java/com/github/robozonky/app/tenant/PowerTenantImplTest.java
+++ robozonky-app/src/test/java/com/github/robozonky/app/tenant/PowerTenantImplTest.java
@@ -59,11 +59,10 @@ class PowerTenantImplTest extends AbstractZonkyLeveragingTest {
         try (tenant) {
             final Statistics s = tenant.call(Zonky::getStatistics);
             assertThat(s).isSameAs(Statistics.empty());
-            verify(a).refresh(any());
         } catch (final Exception e) {
             fail(e);
         }
-        verify(a, times(2)).refresh(any()); // token will be refreshed on close
+        verify(a).refresh(any());
         assertThatThrownBy(() -> tenant.getLoan(1)).isInstanceOf(IllegalStateException.class);
     }
 
