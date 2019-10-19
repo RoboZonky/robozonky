d94dcb120db9912408e9ec50f3c1716bcbc8aea9
Lukáš Petrovický <lukas@petrovicky.net>
2019-10-19 10:38:10 +0200

    Clean up logging

--- robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.sh
+++ robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.sh
@@ -3,6 +3,6 @@ HERE=$(dirname "$0")
 if [ -f ${HERE}/Dist/bin/robozonky-app-${project.version}.jar ]; then
     HERE=$HERE/Dist
 fi
-ROBOZONKY_OPTS="$JAVA_OPTS -XX:+ExitOnOutOfMemoryError -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
+ROBOZONKY_OPTS="$JAVA_OPTS -XX:+ExitOnOutOfMemoryError -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
 #added exec to propagate signals when docker stop is issued, more details https://github.com/RoboZonky/robozonky/pull/305
 exec java $ROBOZONKY_OPTS --module-path ${HERE}/bin --module com.github.robozonky.app "$@"
