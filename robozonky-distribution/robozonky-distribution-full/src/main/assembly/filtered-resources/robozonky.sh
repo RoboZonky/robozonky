94e0e64066ec187aa5d2d76b0ef46b496f57644f
Lukáš Petrovický <lukas@petrovicky.net>
2020-03-13 20:01:51 +0100

    App is built as a shaded JAR to simplify JLinks usage

--- robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.sh
+++ robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+EXEC="robozonky-distribution-full-${project.version}.jar"
 HERE=$(dirname "$0")
-if [ -f ${HERE}/Dist/bin/robozonky-app-${project.version}.jar ]; then
+if [ -f ${HERE}/Dist/$EXEC ]; then
     HERE=$HERE/Dist
 fi
 ROBOZONKY_OPTS="$JAVA_OPTS -XX:+ExitOnOutOfMemoryError -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
 #added exec to propagate signals when docker stop is issued, more details https://github.com/RoboZonky/robozonky/pull/305
-exec java $ROBOZONKY_OPTS -jar ${HERE}/bin/robozonky-app-${project.version}.jar "$@"
+exec java $ROBOZONKY_OPTS -jar ${HERE}/$EXEC "$@"
