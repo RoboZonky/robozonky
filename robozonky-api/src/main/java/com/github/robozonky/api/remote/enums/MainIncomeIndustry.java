d3ee63dd79c64493854c1117197e2d38b3c9897a
Lukáš Petrovický <lukas@petrovicky.net>
2020-05-16 13:05:47 +0200

    Add main income industries

--- robozonky-api/src/main/java/com/github/robozonky/api/remote/enums/MainIncomeIndustry.java
+++ robozonky-api/src/main/java/com/github/robozonky/api/remote/enums/MainIncomeIndustry.java
@@ -26,7 +26,23 @@ import com.github.robozonky.internal.util.json.MainIncomeIndustryDeserializer;
 @JsonbTypeDeserializer(MainIncomeIndustryDeserializer.class)
 public enum MainIncomeIndustry implements BaseEnum {
 
-    UNKNOWN("Neznámý");
+    AUTOMOTIVE_INDUSTRY("Automobilový průmysl"),
+    TRANSPORTATION_AND_STORAGE("Doprava a skladování"),
+    IT("IT"),
+    FINANCIAL_AND_INSURANCE_ACTIVITIES("Peněžnictví a pojišťovnictví"),
+    SERVICE_ACTIVITIES("Služby"),
+    CONSTRUCTION("Stavebnictví"),
+    TELECOMMUNICATION_MEDIA("Telekomunikace, Média"),
+    ACCOMMODATION_AND_FOOD_SERVICE_ACTIVITIES("Ubytování, stravování a pohostinství"),
+    WHOLESALE_AND_RETAIL_TRADE("Velkoobchod a maloobchod"),
+    PUBLIC_ADMINISTRATION_AND_DEFENCE_COMPULSORY_SOCIAL_SECURITY("Veřejná správa a obrana; povin. sociál. zabezpečení"),
+    ELECTRICITY_GAS_STEAM_AND_AIR_CONDITIONING_SUPPLY("Výroba a rozvod elektřiny, plynu, tepla"),
+    EDUCATION("Vzdělávání"),
+    HUMAN_HEALTH_AND_SOCIAL_WORK_ACTIVITIES("Zdravotní a sociální péče"),
+    AGRICULTURE_FORESTRY_AND_FISHING("Zemědělství, lesnictví a rybářství"),
+    MANUFACTURING("Zpracovatelský průmysl"),
+    OTHER_SERVICE_ACTIVITIES("Jiné"),
+    UNKNOWN("Neznámý obor");
 
     private final String code;
 
