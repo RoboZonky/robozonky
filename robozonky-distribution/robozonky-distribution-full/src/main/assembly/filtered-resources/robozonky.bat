05ce5269441141d3d90aa61ca06dabde0a9c9aac
Lukáš Petrovický <lukas@petrovicky.net>
2020-05-29 20:58:13 +0200

    Improve Windows run scripts

--- robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.bat
+++ robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky.bat
@@ -9,7 +9,7 @@ echo Will run %ROBOZONKY_EXECUTABLE% from '%HERE%'.
 
 rem Use JRE bundled with RoboZonky, if available.
 set "CUSTOM_JRE_LOCATION=%HERE%jre\"
-set "JAVA_EXECUTABLE=java"
+set "JAVA_EXECUTABLE=java.exe"
 IF EXIST %CUSTOM_JRE_LOCATION%bin\%JAVA_EXECUTABLE% (
     set "JAVA_EXECUTABLE=%CUSTOM_JRE_LOCATION%bin\%JAVA_EXECUTABLE%"
     set "JAVA_HOME=%CUSTOM_JRE_LOCATION%"
@@ -19,3 +19,6 @@ echo JAVA_HOME set to '%JAVA_HOME%'.
 
 set "ROBOZONKY_OPTS=%JAVA_OPTS% -XX:+ExitOnOutOfMemoryError -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
 %JAVA_EXECUTABLE% %ROBOZONKY_OPTS% -jar %HERE%%ROBOZONKY_EXECUTABLE% %*
+
+rem Don't let the window close if something went wrong.
+pause
