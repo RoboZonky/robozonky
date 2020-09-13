04b08953da9a077304dfab1e8a9ddb5dfe9661b9
Lukáš Petrovický <lukas@petrovicky.net>
2020-09-13 09:44:19 +0200

    Enable running scripts on Windows from "Program Files" (closes #553)

--- robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky-cli.bat
+++ robozonky-distribution/robozonky-distribution-full/src/main/assembly/filtered-resources/robozonky-cli.bat
@@ -18,7 +18,7 @@ echo Will use '%JAVA_EXECUTABLE%' as the Java executable.
 echo JAVA_HOME set to '%JAVA_HOME%'.
 
 set "ROBOZONKY_OPTS=%JAVA_OPTS% -XX:+ExitOnOutOfMemoryError -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true"
-%JAVA_EXECUTABLE% %ROBOZONKY_OPTS% -jar %HERE%%ROBOZONKY_EXECUTABLE% %*
+"%JAVA_EXECUTABLE%" %ROBOZONKY_OPTS% -jar "%HERE%%ROBOZONKY_EXECUTABLE%" %*
 
 rem Don't let the window close if something went wrong.
 pause
