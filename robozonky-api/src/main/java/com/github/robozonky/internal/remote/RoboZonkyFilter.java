234264836a6b69d09597376bd2ddef46949501f1
Lukáš Petrovický <lukas@petrovicky.net>
2021-01-22 15:33:14 +0100

    Log how long a HTTP request took

--- robozonky-api/src/main/java/com/github/robozonky/internal/remote/RoboZonkyFilter.java
+++ robozonky-api/src/main/java/com/github/robozonky/internal/remote/RoboZonkyFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The RoboZonky Project
+ * Copyright 2021 The RoboZonky Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,9 +19,11 @@ package com.github.robozonky.internal.remote;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import javax.ws.rs.client.ClientRequestContext;
@@ -38,19 +40,18 @@ import com.github.robozonky.internal.Settings;
 
 /**
  * Decorates the request with User-Agent and adds some simple request logging.
- * <p>
- * If ever a filter is needed for JAX-RS communication, this class should serve as the base class for that filter
- * .
- * </p>
- * <p>
+ * If ever a filter is needed for JAX-RS communication, this class should serve as the base class for that filter.
  * This class is not thread-safe. Consider using {@link ThreadLocal}.
- * </p>
  */
 class RoboZonkyFilter implements ClientRequestFilter,
         ClientResponseFilter {
 
+    private static final String REQUEST_ID_PROPERTY = "robozonkyRequestId";
+
     // not static, so that filters extending this one get the proper logger class
     protected final Logger logger = LogManager.getLogger(this.getClass());
+    // In case the same filter is used multiple times, we add support for timing multiple requests.
+    private final Map<String, Long> requestStartNanotimes = new HashMap<>(1);
     private final Map<String, Object[]> queryParams = new TreeMap<>();
     private final Map<String, String> requestHeaders = new TreeMap<>();
     private Map<String, String> responseHeaders = Collections.emptyMap();
@@ -97,6 +98,10 @@ class RoboZonkyFilter implements ClientRequestFilter,
         requestHeaders.forEach((k, v) -> clientRequestContext.getHeaders()
             .putSingle(k, v));
         clientRequestContext.setUri(rebuild(clientRequestContext.getUri()));
+        var requestId = UUID.randomUUID()
+            .toString();
+        clientRequestContext.setProperty(REQUEST_ID_PROPERTY, requestId);
+        requestStartNanotimes.put(requestId, System.nanoTime());
         logger.trace("Request {} {}.", clientRequestContext.getMethod(), clientRequestContext.getUri());
     }
 
@@ -114,11 +119,23 @@ class RoboZonkyFilter implements ClientRequestFilter,
     @Override
     public void filter(final ClientRequestContext clientRequestContext,
             final ClientResponseContext clientResponseContext) throws IOException {
-        logger.debug("HTTP {} Response from {}: {} {}.", clientRequestContext.getMethod(),
-                clientRequestContext.getUri(), clientResponseContext.getStatus(),
-                clientResponseContext.getStatusInfo()
-                    .getReasonPhrase());
-        final String responseEntity = getResponseEntity(clientResponseContext);
+        var currentNanotime = System.nanoTime();
+        var requestId = (String) clientRequestContext.getProperty(REQUEST_ID_PROPERTY);
+        if (requestId == null) { // Happens during testing, where a lot of things are mocked.
+            logger.debug("HTTP {} Response from {}: {} {}.", clientRequestContext.getMethod(),
+                    clientRequestContext.getUri(), clientResponseContext.getStatus(),
+                    clientResponseContext.getStatusInfo()
+                        .getReasonPhrase());
+        } else {
+            var startNanotime = requestStartNanotimes.remove(requestId);
+            var millisSpent = (currentNanotime - startNanotime) / 1000 / 1000;
+            logger.debug("HTTP {} Response from {}: {} {} ({} ms).", clientRequestContext.getMethod(),
+                    clientRequestContext.getUri(), clientResponseContext.getStatus(),
+                    clientResponseContext.getStatusInfo()
+                        .getReasonPhrase(),
+                    millisSpent);
+        }
+        var responseEntity = getResponseEntity(clientResponseContext);
         if (clientResponseContext.getStatus() == 400 && responseEntity.contains("invalid_token")) {
             // Zonky is dumb and throws 400 when it should throw 401
             clientResponseContext.setStatus(401);
